9efd7ddbd1f9acaabb1692278f610032614ae510
manuel blanco <mandresblac@gmail.com>
2022-09-20 19:20:14 -0500

    commit 2

--- js/indexDom.js
+++ js/indexDom.js
@@ -1,3 +1,4 @@
+import countdown from "./cuentaRegresiva.js";
 import hamburgerMenu from "./menuHamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { shortCuts } from "./teclado.js";
@@ -14,6 +15,12 @@ d.addEventListener("DOMContentLoaded", e => {
 
     /* Para ejecutar la alarma */
     alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+
+    countdown(
+        "countdown", 
+        "Sep 20,2022 19:19:00", 
+        "feliz cumpleaños amigo y docente digital 🤓"
+        );
 });
 
 //El teclado tiene 3 eventos: keydown cuando oprimimos una tecla, keypress mientras tengamos oprimida una tecla y keyup cuando se deja de oprimir una tecla, es decir, cuando soltamos la tecla.
